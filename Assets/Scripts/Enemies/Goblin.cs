6fcf270bb70a95fe8d63d6b29b68b0ee14816304
Ruteski <lruteski@gmail.com>
2022-05-28 17:34:22 -0300

    inimigo2: animacoes, incrementada IA para seguir o player

--- Assets/Scripts/Enemies/Goblin.cs
+++ Assets/Scripts/Enemies/Goblin.cs
@@ -8,16 +8,19 @@ public class Goblin : MonoBehaviour
     [SerializeField] private float _maxVision = 5f;
     [SerializeField] private bool _isFront = false;
     [SerializeField] private Transform _point;
+    [SerializeField] private Transform _pointBehind;
     [SerializeField] private bool _isRight = true;
     [SerializeField] private float _stopDistance;
-
+    
     private Rigidbody2D _rb;
+    private Animator _animator;
     private Vector2 _direction;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
 
         if (_isRight) {//vira pra direita
             transform.eulerAngles = new Vector2(0, 0);
@@ -28,19 +31,15 @@ public class Goblin : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate() {
         GetPlayer();
         OnMove();
     }
 
     private void OnMove() {
-        if (_isFront){ 
+        if (_isFront){
+            _animator.SetInteger("Transition", 1);
+
             if (_isRight) {//vira pra direita
                 transform.eulerAngles = new Vector2(0, 0);
                 _direction = Vector2.right;
@@ -55,6 +54,7 @@ public class Goblin : MonoBehaviour
 
     private void GetPlayer() {               //origem,        direcaov     , distancia
         RaycastHit2D hit = Physics2D.Raycast(_point.position, _direction, _maxVision);
+        
 
         if (hit.collider != null) {
             if (hit.transform.CompareTag("Player")) {
@@ -67,8 +67,19 @@ public class Goblin : MonoBehaviour
                     _isFront = false;
                     _rb.velocity = Vector2.zero;
 
+                    _animator.SetInteger("Transition", 2);
+
                     hit.transform.GetComponent<Player>().OnHit();
                 }
+            }
+        }
+
+        RaycastHit2D behindhit = Physics2D.Raycast(_pointBehind.position, -_direction, _maxVision);
+
+        if (behindhit.collider != null) {
+            if (behindhit.transform.CompareTag("Player")) {
+                //player nas costas do inimigo
+                _isRight = !_isRight;
             }
         }
     }
@@ -77,6 +88,7 @@ public class Goblin : MonoBehaviour
     private void OnDrawGizmos() {
         //Gizmos.DrawRay(_point.position, _direction * _maxVision);
         Debug.DrawRay(_point.position, _direction * _maxVision, Color.red);
+        Debug.DrawRay(_pointBehind.position, -_direction * _maxVision, Color.green);
     }
 
     //esse metodo só aparece os gismos quando eu seleciono o objeto que tenha gismos para aparecer
